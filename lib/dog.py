c756f05e5b9c1f8998d5de042d1372d35f91bcdc
WAHU0508 <sharongikenye@gmail.com>
2024-08-27 12:17:42 +0300

    Attributes and properties in person class

--- lib/dog.py
+++ lib/dog.py
@@ -13,25 +13,19 @@ APPROVED_BREEDS = [
 
 class Dog:
     def __init__(self, name="Unknown", breed="Mastiff"):
-        self._name = None
-        self._breed = None
         self.name = name
         self.breed = breed
     def get_name(self):
-        print("Retrieving name:")
         return self._name
     def set_name(self, name):
-        if isinstance(name, str) and (0 < len(name) < 25):
-            print(f"Setting name {name}")
+        if isinstance(name, str) and 0 < len(name) <= 25:
             self._name = name
         else:
             print("Name must be string between 1 and 25 characters.")
     def get_breed(self):
-        print("Retrieving breed:")
         return self._breed
     def set_breed(self, breed):
-        if (breed in APPROVED_BREEDS):
-            print(f"Setting breed {breed}:")
+        if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
             print("Breed must be in list of approved breeds.")
